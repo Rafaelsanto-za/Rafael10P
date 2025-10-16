e2f2b814e673e72ab7545f0838e3171bcde784df
G.KAI <rafaebelem2020@gmail.com>
2025-10-16 16:02:43 +0100

    Add files via upload

---
+++ pochaestiaca.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	int num, maior = -2147483648, menor = 2147483647;
+	do {
+
+		cout << "fale um numero  ";
+		cin >> num;
+
+		if (num > maior) {
+			maior = num;
+		}
+		if (num < menor && num != 0) {
+			menor = num;
+		}
+
+	} while (num != 0);
+	cout << "Numero menor: " << maior << "\n";
+
+	cout << "Numero maior: " << menor << "\n";
+	return 0;
+}
