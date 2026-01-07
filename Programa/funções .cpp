8879c588eae64a39b9954ddd723b0051fd72f9c5
G.KAI <rafaebelem2020@gmail.com>
2026-01-07 13:22:28 +0000

    Update funções .cpp

--- Programa/funções .cpp
+++ Programa/funções .cpp
@@ -8,43 +8,71 @@ using namespace std;
 int calculos(int n) 
 {
 	
-	
-
-
+	return 0;
 
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Portuguese");
-	srand(time(0));
+	
 	int i,numero,valor,n;
 
-	for (i = 0; i < 10; i++) {
-		cout << "insira 10 numeros\n";
+	for (i = 0; i < 10; i++) 
+	{
+		cout << "\ninsira 10 numeros\n";
 		cin >> n;
 
-		if (n > 10 || n < 0)
+		if (n > 2147483647 || n < -2147483648)
 		{
-			cout << " o nome esta incorreto";
+			cout << " O Numero esta incorreto\n";
+			cout << " Teste novamente:\n ";
 		}
 //ingr
-		cout << "------------------Menu------------------";
-		cout << "1-calcula a media dos numeros\n";
-		cin >> numero;
-		cout << "2-achar o menor\n ";
-		cin >> numero;
-		cout << "3-Achar o maior\n ";
-		cin >> numero;
-		cout << "0-Sair\n ";
-		cout << "----------------------------------------";
+//Nesse programa, use 3 funções para os pontos 1, 2 e 3.​
 
+		
 		valor = calculos(n);
-	    
 
 
+		
+		
 	}
+	do {
+		system("cls");
+
+		cout << "\n------------------Menu------------------\n";
+
+		cout << "\n1-calcula a media dos numeros\n";
+
+		cout << "\n2-achar o menor\n ";
+
+		cout << "\n3-Achar o maior\n ";
+
+		cout << "\n0-Sair\n ";
+
+		cout << "\n----------------------------------------\n";
+
+		cout << "escolha uma opeção\n";
+
+		cin >> numero;
+
+		switch (numero) {
+		case 1:
+			cout << "A media dosnumeros são:" <<      <<;
+
+
+		}
+
+	} while (numero != 0);
 	
 
-	return 0;
+	
+
+	
+
+
+	
+
+	return	0;
 }
