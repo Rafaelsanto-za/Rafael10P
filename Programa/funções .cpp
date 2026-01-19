8fbc23c9aedac1890653c87ea19813127c4ffb4a
G.KAI <rafaebelem2020@gmail.com>
2026-01-19 13:17:49 +0000

    Update funções .cpp

--- Programa/funções .cpp
+++ Programa/funções .cpp
@@ -1,78 +1,130 @@
 #include <iostream>
-#include <string>
 #include <locale.h>
-#include <cstdlib>
-#include <time.h>
 using namespace std;
 
-int calculos(int n) 
+void Enviardinheiro(int saldo)
 {
-	
-	return 0;
+    int enviar = 0;
 
+    if (enviar > saldo) {
+        cout << "Saldo insuficiente!\n";
+    }
+    else {
+        saldo -= enviar;
+        cout << "Envio realizado com sucesso!\n";
+    }
+    cin.ignore();
+    cin.get();
+    
 }
 
-int main()
-{
-	setlocale(LC_ALL, "Portuguese");
-	
-	int i,numero,valor,n;
+void Enviardinheiro(int deposito) {
+    int saldo;
 
-	for (i = 0; i < 10; i++) 
-	{
-		cout << "\ninsira 10 numeros\n";
-		cin >> n;
+    if (deposito >= 2000) {
+        cout << "Cuidado para não afundares o banco com tanto dinheiro!\n";
+    }
 
-		if (n > 2147483647 || n < -2147483648)
-		{
-			cout << " O Numero esta incorreto\n";
-			cout << " Teste novamente:\n ";
-		}
-//ingr
-//Nesse programa, use 3 funções para os pontos 1, 2 e 3.​
+    saldo += deposito;
+    cout << "Depósito realizado!\n";
+    cin.ignore();
+    cin.get();
 
-		
-		valor = calculos(n);
-
-
-		
-		
-	}
-	do {
-		system("cls");
-
-		cout << "\n------------------Menu------------------\n";
-
-		cout << "\n1-calcula a media dos numeros\n";
-
-		cout << "\n2-achar o menor\n ";
-
-		cout << "\n3-Achar o maior\n ";
-
-		cout << "\n0-Sair\n ";
-
-		cout << "\n----------------------------------------\n";
-
-		cout << "escolha uma opeção\n";
-
-		cin >> numero;
-
-		switch (numero) {
-		case 1:
-			cout << "A media dosnumeros são:" <<      <<;
-
-
-		}
-
-	} while (numero != 0);
-	
-
-	
-
-	
-
-
-	
-
-	return	0;
 }
+void Enviardinheiro(int debito) {
+    int saldo;
+    if (debito > saldo) {
+        cout << "O Débito é superior ao saldo!\n";
+    }
+    else {
+        saldo -= debito;
+        cout << "Débito realizado!\n";
+    }
+    cin.ignore();
+    cin.get();
+}
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    int resposta = -1;
+    int deposito = 0;
+    int debito = 0;
+    int enviar = 0;
+    int saldo = 1000;
+   
+
+    cout << "Deseja iniciar? digite a senha: ";
+    cin >> resposta;
+
+    if (resposta != 12345) {
+        cout << "Programa encerrado.\n";
+        return 0;
+    }
+
+    do {
+        system("cls");
+
+        cout << "\n------------------ Menu ------------------\n";
+        cout << "1 - Verificar Saldo\n";
+        cout << "2 - Enviar Dinheiro\n";
+        cout << "3 - Depósito\n";
+        cout << "4 - Débito\n";
+        cout << "0 - Sair\n";
+        cout << "------------------------------------------\n";
+        cout << "Escolha uma opção: ";
+        cin >> resposta;
+
+        switch (resposta) {
+
+        case 1:
+            system("cls");
+            cout << "\n------ Verificação de Saldo ------\n";
+            cout << "Saldo atual: " << saldo << " \n";
+            cin.ignore();
+            cin.get();
+            break;
+
+        case 2:
+            system("cls");
+            cout << "\n------ Enviar Dinheiro ------\n";
+            cout << "Valor a enviar: ";
+            cin >> enviar;
+            Enviardinheiro(saldo);
+
+            
+            break;
+
+        case 3:
+            system("cls");
+            cout << "\n------ Depósito ------\n";
+            cout << "Valor a depositar: ";
+            cin >> deposito;
+            Enviardinheiro(deposito);
+          
+            break;
+
+        case 4:
+            system("cls");
+            cout << "\n------ Débito ------\n";
+            cout << "Valor do débito: ";
+            cin >> debito;
+            Enviardinheiro(debito);
+            break;
+
+        case 0:
+            cout << "A sair...\n";
+            break;
+
+        default:
+            cout << "Opção inválida!\n";
+            cin.ignore();
+            cin.get();
+            break;
+        }
+
+    } while (resposta != 0);
+
+    return 0;
+}
+
